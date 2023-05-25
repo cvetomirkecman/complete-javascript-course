e32fa92c7438c6f6247241f419015883ba7b7f14
Kecolol <cvejka.kecman@gmail.com>
2023-05-25 12:35:10 +0200

    day 9

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,7 +4,6 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
-console.log(btnsOpenModal)
 
 const closeModal = function () {
   modal.classList.add('hidden')
@@ -12,7 +11,6 @@ const closeModal = function () {
 }
 
 const openModal = function () {
-  console.log('Button clicked')
   modal.classList.remove('hidden')
   overlay.classList.remove('hidden')
 }
@@ -24,3 +22,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal)
 
 overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+  }
+})
